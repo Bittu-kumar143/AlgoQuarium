79c2e6c57ca3e34b2ae0f02809596feb5bd29b36
Bittu kumar <124612467+Bittu-kumar143@users.noreply.github.com>
2026-01-08 22:15:49 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="bg-transparent shadow-md p-4 text-center">
       <p className="text-sm text-[#b0b8c4]">
-      © 2025 Algoquarium. Built with ☕ and ❤️ by Ashish Rolan — Empowering your DSA journey, one visualization at a time.
+      © 2025 Algoquarium. Built with ☕ and ❤️ by Bittu kumar — Empowering your DSA journey, one visualization at a time.
       </p>
     </footer>
   );
